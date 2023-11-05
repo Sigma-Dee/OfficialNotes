aba7033ea62ca49a40cddf12f071623987fa2913
Sigma-Dee <devtee9@gmail.com>
2023-11-05 05:32:22 -0500

    Update UI and fix adapter

--- lib/screens/calendar.dart
+++ lib/screens/calendar.dart
@@ -1,10 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-class CalendarScreen extends StatelessWidget {
+class CalendarScreen extends StatefulWidget {
   const CalendarScreen({super.key});
 
   @override
+  State<CalendarScreen> createState() => _CalendarScreenState();
+}
+
+class _CalendarScreenState extends State<CalendarScreen> {
+  DateTime today = DateTime.now();
+
+  void onDaySelected(DateTime day, DateTime focusedDay) {
+    setState(() {
+      today = day;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Text(
+              'Selected Day = ${today.toString().split(" ")[0]}',
+            ),
+            Container(
+              child: TableCalendar(
+                locale: 'en_US',
+                headerStyle: const HeaderStyle(
+                  formatButtonVisible: false,
+                  titleCentered: true,
+                ),
+                availableGestures: AvailableGestures.all,
+                focusedDay: today,
+                firstDay: DateTime.utc(1998, 09, 09),
+                lastDay: DateTime.utc(2030, 9, 9),
+                selectedDayPredicate: (day) => isSameDay(day, today),
+                onDaySelected: onDaySelected,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
